16918602b494b6e0bb2b95d7271e7ce587a084ef
sunflow-er <antjd03170@gmail.com>
2023-11-15 14:52:21 +0900

    recycler_view

--- app/src/main/java/org/javaapp/communityapp/BoardMainFragment.kt
+++ app/src/main/java/org/javaapp/communityapp/BoardMainFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.javaapp.communityapp.databinding.BoardMainRecyclerItemBinding
 import org.javaapp.communityapp.databinding.FragmentBoardMainBinding
 
 class BoardMainFragment : Fragment() {
@@ -22,8 +26,50 @@ class BoardMainFragment : Fragment() {
         // Inflate the layout for this fragment
 
         boardMainFragmentBinding = FragmentBoardMainBinding.inflate(inflater)
+        boardMainFragmentBinding.boardMainToolbar.title = "게시판 이름"
+
+        /*--------------------------------------------recycler View------------------------------------------------*/
+        val boardMainRecyclerAdapter = BoardMainRecyclerAdapter()
+        boardMainFragmentBinding.boardMainRecycler.adapter = boardMainRecyclerAdapter
+
+        boardMainFragmentBinding.boardMainRecycler.layoutManager = LinearLayoutManager(requireContext())
+        boardMainFragmentBinding.boardMainRecycler.addItemDecoration(DividerItemDecoration(requireContext(), 1))
+        /*--------------------------------------------recycler View------------------------------------------------*/
 
         return boardMainFragmentBinding.root
     }
 
+    /*--------------------------------------------recycler View------------------------------------------------*/
+    inner class BoardMainRecyclerAdapter : RecyclerView.Adapter<BoardMainRecyclerAdapter.ViewHolderClass>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
+            val boardMainRecyclerItemBinding = BoardMainRecyclerItemBinding.inflate(layoutInflater)
+            val holder = ViewHolderClass (boardMainRecyclerItemBinding)
+
+            val layoutParams = RecyclerView.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            boardMainRecyclerItemBinding.root.layoutParams = layoutParams
+            boardMainRecyclerItemBinding.root.setOnClickListener(holder)
+
+            return holder
+        }
+
+        override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
+
+        }
+
+        override fun getItemCount(): Int {
+            return 10
+        }
+
+        inner class ViewHolderClass(boardMainRecyclerItemBinding: BoardMainRecyclerItemBinding)
+            : RecyclerView.ViewHolder(boardMainRecyclerItemBinding.root), View.OnClickListener {
+            override fun onClick(v: View?) {
+
+            }
+        }
+    }
+    /*--------------------------------------------recycler View------------------------------------------------*/
 }
